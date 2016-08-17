e450ca9ad1f63f7da2b4a7b7ae2f8c258a3a9391
tarlepp <tarmo.leppanen@protacon.com>
2016-08-17 14:13:30 +0300

    Changes to "about" module.

--- src/modules/about/about.controller.js
+++ src/modules/about/about.controller.js
@@ -1,2 +1,45 @@
+/**
+ * @ngInject
+ */
 export default class AboutController {
+  constructor($window) {
+    this.$window = $window;
+
+    this.items = [
+      {
+        title: 'Angular.js',
+        image: require('./img/angularjs-logo.png'),
+        url: 'https://angularjs.org/',
+      },
+      {
+        title: 'Angular Material',
+        image: require('./img/angular-materia-logo.png'),
+        url: 'https://material.angularjs.org',
+      },
+      {
+        title: 'Webpack',
+        image: require('./img/what-is-webpack.png'),
+        url: 'https://webpack.github.io/',
+      },
+      {
+        title: 'Babel',
+        image: require('./img/babel-logo.png'),
+        url: 'https://babeljs.io/',
+      },
+      {
+        title: 'Node Sass',
+        image: require('./img/libsass-logo.png'),
+        url: 'https://www.npmjs.com/package/node-sass',
+      },
+      {
+        title: 'Json Web Token',
+        image: require('./img/jwt-logo.png'),
+        url: 'https://jwt.io/',
+      },
+    ];
+  }
+
+  open(item) {
+    this.$window.open(item.url, '_blank');
+  }
 }
--- src/modules/about/about.module.js
+++ src/modules/about/about.module.js
@@ -1,6 +1,8 @@
 import angular from 'angular';
 import routes from './about.routes';
 
+import './about.scss';
+
 export default angular
   .module('modules.about', [])
   .run(routes)
