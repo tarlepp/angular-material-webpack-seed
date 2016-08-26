6cdbdc98564de4132ff566a78fd6148ebe5c9b2d
tarlepp <tarmo.leppanen@protacon.com>
2016-08-26 03:43:58 +0300

    Added menu item service.

---
+++ src/core/services/menuItem.service.js
@@ -0,0 +1,97 @@
+import _ from 'lodash';
+
+/**
+ * @ngInject
+ */
+export default class MenuItemService {
+  /**
+   * Constructor of the class.
+   *
+   * @param {$state}      $state
+   * @param {$mdSidenav}  $mdSidenav
+   * @param {AuthService} AuthService
+   * @param {UserService} UserService
+   * @param {UserRoles}   UserRoles
+   */
+  constructor(
+    $state, $mdSidenav,
+    AuthService, UserService, UserRoles
+  ) {
+    this.$state = $state;
+    this.$mdSidenav = $mdSidenav;
+    this.authService = AuthService;
+    this.userService = UserService;
+    this.userRoles = UserRoles;
+
+    // Actual menu items
+    this.items = [
+      {
+        title: 'About',
+        state: 'modules.about',
+        access: this.userRoles.ROLE_ANON,
+      },
+      {
+        title: 'Profile',
+        state: 'auth.profile',
+        access: this.userRoles.ROLE_LOGGED,
+      },
+      {
+        title: 'Login',
+        state: 'auth.login',
+        access: this.userRoles.ROLE_ANON,
+        logged: false,
+      },
+      {
+        title: 'Logout',
+        state: 'auth.logout',
+        access: this.userRoles.ROLE_LOGGED,
+      },
+    ];
+  }
+
+  /**
+   * Getter method for all menu items.
+   *
+   * @returns {object[]}
+   */
+  getItems() {
+    const iterator = (item) => {
+      if ({}.hasOwnProperty.call(item, 'items')) {
+        item.items = _.filter(item.items, iterator);
+
+        if (item.items.length === 0) {
+          return false;
+        }
+      }
+
+      if ({}.hasOwnProperty.call(item, 'logged')
+        && !item.logged
+      ) {
+        return !this.userService.getProfile();
+      }
+
+      return this.authService.authorize(item.access);
+    };
+
+    return _.filter(this.items, iterator);
+  }
+
+  /**
+   * Method to change application state to another one.
+   *
+   * @param {object}  item
+   * @param {object}  [params]
+   */
+  goToPage(item, params = {}) {
+    let parameters = {};
+
+    if (_.isEmpty(params)
+      && {}.hasOwnProperty.call(item, 'params')
+      && !_.isEmpty(item.params)
+    ) {
+      parameters = item.params;
+    }
+
+    this.$state.go(item.state, parameters);
+  }
+}
--- src/core/services/services.module.js
+++ src/core/services/services.module.js
@@ -1,7 +1,9 @@
 import angular from 'angular';
 import HttpStatusService from './httpStatus.service';
+import MenuItemService from './menuItem.service';
 
 export default angular
   .module('core.services', [])
   .service('HttpStatusService', HttpStatusService)
+  .service('MenuItemService', MenuItemService)
   .name;
