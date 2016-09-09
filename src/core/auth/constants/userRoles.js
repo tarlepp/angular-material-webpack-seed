84aeff651ead3b686c9abb2703560f1fcca60b5a
tarlepp <tarmo.leppanen@protacon.com>
2016-09-09 21:05:27 +0300

    Added missing comments.

--- src/core/auth/constants/userRoles.js
+++ src/core/auth/constants/userRoles.js
@@ -1,3 +1,16 @@
+/**
+ * @desc  This file contains used user roles within application. These are used to authorize user access to application
+ *        routes - for more information see one of those ***.routes.js files on sources.
+ * @name  UserRoles
+ *
+ * @type {{
+ *    ROLE_ANON: string,
+ *    ROLE_LOGGED: string,
+ *    ROLE_USER: string,
+ *    ROLE_ADMIN: string,
+ *    ROLE_ROOT: string
+ *  }}
+ */
 const UserRoles = {
   ROLE_ANON: 'ROLE_ANON',
   ROLE_LOGGED: 'ROLE_LOGGED',
