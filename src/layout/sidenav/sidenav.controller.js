3448acb7dcd169a9c610f301502d9dad775a68cd
tarlepp <tarmo.leppanen@protacon.com>
2016-09-10 15:54:46 +0300

    Refactored sidenav controller and template.

--- src/layout/sidenav/sidenav.controller.js
+++ src/layout/sidenav/sidenav.controller.js
@@ -5,31 +5,27 @@ export default class SidenavController {
   /**
    * Constructor of the class.
    *
-   * @param {$scope}          $scope
-   * @param {$state}          $state
-   * @param {$mdSidenav}      $mdSidenav
-   * @param {MenuItemService} MenuItemService
+   * @param {$scope}                  $scope
+   * @param {ui.router.state.$state}  $state
+   * @param {$mdSidenav}              $mdSidenav
+   * @param {MenuItemService}         MenuItemService
    */
   constructor(
     $scope, $state, $mdSidenav,
     MenuItemService
   ) {
-    this.$state = $state;
     this.$mdSidenav = $mdSidenav;
-    this.menuItemService = MenuItemService;
 
     // Attach includes function to controller
-    this.isActive = this.$state.includes;
+    this.isActive = $state.includes;
 
-    // For now we need a watcher for actual menu items
+    // For now we need a watcher for actual menu items - note that is $rootScope variable
     $scope.$watch('isAuthenticated', () => {
-      this.items = this.menuItemService.getItems();
+      this.items = MenuItemService.getItems();
     });
   }
 
-  /**
-   * Method to hide left side navigation bar.
-   */
+  // Method to hide left side navigation bar.
   hideSideMenu() {
     this.$mdSidenav('left').close();
   }
