79f3924919098fcb19ad3ba06924dd06e61e29cb
tarlepp <tarmo.leppanen@protacon.com>
2016-09-11 19:38:54 +0300

    Changed dependencies module name.

--- src/dependencies/dependencies.module.js
+++ src/dependencies/dependencies.module.js
@@ -1,3 +1,4 @@
+// Imports
 import angular from 'angular';
 import angularAnimate from 'angular-animate';
 import angularAria from 'angular-aria';
@@ -14,7 +15,7 @@ import config from './dependencies.config';
  * @ngInject
  */
 export default angular
-  .module('dependencies', [
+  .module('app.dependencies', [
     angularAnimate, angularAria, angularJWT, angularLoadingBar, angularMaterial, angularSanitize,
     ngstorage.name, // see https://github.com/gsklee/ngStorage/pull/159
     uiRouter,
